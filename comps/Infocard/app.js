fd11057926f93d4f0ba691e161c1d5c77320d7f7
Your Name <you@example.com>
2020-10-08 11:07:40 -0700

    styled infocard component

--- comps/Infocard/app.js
+++ comps/Infocard/app.js
@@ -1,30 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Infocard = styled.div`
+const InfoWrapper = styled.div`
     background: #FFFFFF;
     box-shadow: 6px 5px 21px rgba(10, 23, 34, 0.12);
     border-radius: 10px 10px 10px 0px;
+    max-width: 215px;
+    min-height: 147px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const ContentWrapper = styled.div`
+    max-width: 182px;
+    margin-left: 21px;
 `;
 
 const Header = styled.div`
-
+    font-style: normal;
+    font-weight: normal;
+    font-size: 27px;
+    color: #AFCF6B;
+    margin-top: 24px;
+    margin-bottom: 11px;
 `;
 
 const Content = styled.div`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    color: #000000;
 
 `;
 
-const Infocard = ({H1, text})=>{
+const Infocard = ({Heading, text})=>{
   return <InfoWrapper>
-      <Header>{H1}</Header>
-      <Content>{text}</Content>
+      <ContentWrapper>
+        <Header>{Heading}</Header>
+        <Content>{text}</Content>
+      </ContentWrapper>
   </InfoWrapper>
 }
 
 Infocard.defaultProps = {
-  H1: "Information",
+  Heading: "Information",
   text: "Hover your mouse over any card for a definition."
 }
 
-export default Infocard;
+export default Infocard; 
---
+++ stories/Infocard.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Infocard from '../comps/Infocard/app';
+
+export default {
+  title: 'Info',
+  component: Infocard
+};
+
+export const myInfo = () => 
+<Infocard/>
