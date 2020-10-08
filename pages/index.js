0396b8027d47141aab2b0b883b237187a374a490
mabeliang <mabeliang@hotmail.com>
2020-10-08 12:21:33 -0700

    started page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
 import React, {useState} from 'react';
+import Header from '../comps/Header';
+import BigCard from '../comps/BigCard';
+import Button from '../comps/Button';
+import Infocard from '../comps/Infocard/app';
+import Slidercard from '../comps/Slidercard/app';
+import PercentCard from '../comps/PercentCard';
+
 export default function Home() {
+    return <div>
+        <Header/>
+        <BigCard />
+        <Button />
+        <Infocard />
+        <Slidercard Num="$52,023" MinValue="$46,900" MaxValue="$79,100" />
+    </div>
 }
